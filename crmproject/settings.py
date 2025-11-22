ff855d4a391703d3f26237b787e20954a336edbe
enamul-asad <ehaq14520@gmail.com>
2025-11-22 22:21:24 +0530

    Update settings.py

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://marc-lab-crm.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
